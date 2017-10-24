31deec157baf35bd7ca1c3bdf1ff1569c9f4af3d
Nathan Pawloski <natepawloski@gmail.com>
2017-10-24 11:30:17 -0400

    added controller

---
+++ Suggestion_Box/Models/SuggestionModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Suggestion_Box.Models
+{
+    public class SuggestionModel
+    {
+
+        [Key]
+        public int ID { get; set; }
+
+        public string Comment { get; set; }
+
+        //public string Topic { get; set; }
+
+        public string Topic
+        {
+            get { return this.Topic; }
+            set { this.Topic = value; }
+        }
+
+    }
+}
